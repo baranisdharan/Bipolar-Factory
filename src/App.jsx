909c5b7e32ca3a6212882aed63c19bacda2f4fa8
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-08-26 15:31:16 +0530

    App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import NoteList from './NoteList';
+import NoteEditor from './NoteEditor';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (newNote) => {
+    setNotes([...notes, newNote]);
+  };
+
+  const updateNote = (index, updatedNote) => {
+    const updatedNotes = [...notes];
+    updatedNotes[index] = updatedNote;
+    setNotes(updatedNotes);
+  };
+
+  const deleteNote = (index) => {
+    const updatedNotes = notes.filter((_, i) => i !== index);
+    setNotes(updatedNotes);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Note Taking App</h1>
+      <NoteList notes={notes} updateNote={updateNote} deleteNote={deleteNote} />
+      <NoteEditor addNote={addNote} />
+    </div>
+  );
 }
 
-export default App
+export default App;
