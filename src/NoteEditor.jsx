607475ce51462ffb49349cbdc57f2963d711baed
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-08-26 15:39:33 +0530

    NoteEditor

--- src/NoteEditor.jsx
+++ src/NoteEditor.jsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 function NoteEditor({ addNote }) {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [category, setCategory] = useState('Work'); // Default category
 
   const handleSave = () => {
     if (title && content) {
-      addNote({ title, content });
+      addNote({ title, content, category });
       setTitle('');
       setContent('');
+      setCategory('Work');
     }
   };
 
@@ -26,6 +28,15 @@ function NoteEditor({ addNote }) {
         value={content}
         onChange={(e) => setContent(e.target.value)}
       />
+      <label>Category: </label>
+      <select
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value="Work">Work</option>
+        <option value="Personal">Personal</option>
+        {/* Add more categories here */}
+      </select>
       <button onClick={handleSave}>Save Note</button>
     </div>
   );
