a2a77363b86cc025286417efc1839e1ea437da87
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-08-27 22:14:04 +0530

    styles

--- src/NoteEditor.jsx
+++ src/NoteEditor.jsx
@@ -15,42 +15,43 @@ function NoteEditor({ addNote }) {
   };
 
   return (
-    <div>
+    <div style={{ textAlign: 'center', marginBottom: '20px' }}>
       <h2>Create a Note</h2>
-      <div>
-        <label htmlFor="title">Title: </label>
-        <input
-          type="text"
-          id="title"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{ border: '1px solid black', padding: '20px', width: '300px' }}>
+          <label htmlFor="title">Title: </label>
+          <input
+            type="text"
+            id="title"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <br />
+          <br />
+          <label htmlFor="content">Content: </label>
+          <input
+            id="content"
+            placeholder="Text"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <br /><br />
+          <label htmlFor="category">Category: </label>
+          <select
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="Work">Work</option>
+            <option value="Personal">Personal</option>
+            {/* Add more categories here */}
+          </select>
+          <br /><br />
+          <button onClick={handleSave}>Save Note</button>
+        </div>
       </div>
-      <div>
-        <label htmlFor="content">Content: </label>
-        <input
-          id="content"
-          placeholder="Text"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="category">Category: </label>
-        <select
-          id="category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="Work">Work</option>
-          <option value="Personal">Personal</option>
-          {/* Add more categories here */}
-        </select>
-      </div>
-      <div>
-        <button onClick={handleSave}>Save Note</button>
-      </div>
+      <hr />
     </div>
   );
 }
--- src/NoteList.jsx
+++ src/NoteList.jsx
@@ -29,80 +29,84 @@ function NoteList({ notes, updateNote, deleteNote }) {
 
   return (
     <div>
-      <div>
-        <label>Filter by Category: </label>
-        <select
-          value={selectedCategory}
-          onChange={(e) => setSelectedCategory(e.target.value)}
-        >
-          <option value="All">All</option>
-          <option value="Work">Work</option>
-          <option value="Personal">Personal</option>
-          {/* Add more categories here */}
-        </select>
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px' }}>
+        <div>
+          <label>Filter by Category: </label>
+          <select
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategory(e.target.value)}
+          >
+            <option value="All">All</option>
+            <option value="Work">Work</option>
+            <option value="Personal">Personal</option>
+            {/* Add more categories here */}
+          </select>
+        </div>
+        <div>
+          <label>Search: </label>
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
       </div>
-      <div>
-        <label>Search: </label>
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Content</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentNotes.map((note, index) => (
-            <tr key={index}>
-              <td>
-                {editingIndex === index ? (
-                  <input
-                    type="text"
-                    placeholder="Title"
-                    value={note.title}
-                    onChange={(e) =>
-                      updateNote(index, { ...note, title: e.target.value })
-                    }
-                  />
-                ) : (
-                  note.title
-                )}
-              </td>
-              <td>
-                {editingIndex === index ? (
-                  <input
-                    placeholder="Content"
-                    value={note.content}
-                    onChange={(e) =>
-                      updateNote(index, { ...note, content: e.target.value })
-                    }
-                  />
-                ) : (
-                  note.content
-                )}
-              </td>
-              <td>
-                {editingIndex === index ? (
-                  <button onClick={() => setEditingIndex(-1)}>Save</button>
-                ) : (
-                  <button onClick={() => handleEdit(index)}>Edit</button>
-                )}
-              </td>
-              <td>
-                <button onClick={() => deleteNote(index)}>Delete</button>
-              </td>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <table style={{ border: '1px solid black', width: '60%' }}>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Content</th>
+              <th>Edit</th>
+              <th>Delete</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <div>
+          </thead>
+          <tbody>
+            {currentNotes.map((note, index) => (
+              <tr key={index}>
+                <td>
+                  {editingIndex === index ? (
+                    <input
+                      type="text"
+                      placeholder="Title"
+                      value={note.title}
+                      onChange={(e) =>
+                        updateNote(index, { ...note, title: e.target.value })
+                      }
+                    />
+                  ) : (
+                    note.title
+                  )}
+                </td>
+                <td>
+                  {editingIndex === index ? (
+                    <input
+                      placeholder="Content"
+                      value={note.content}
+                      onChange={(e) =>
+                        updateNote(index, { ...note, content: e.target.value })
+                      }
+                    />
+                  ) : (
+                    note.content
+                  )}
+                </td>
+                <td>
+                  {editingIndex === index ? (
+                    <button onClick={() => setEditingIndex(-1)}>Save</button>
+                  ) : (
+                    <button onClick={() => handleEdit(index)}>Edit</button>
+                  )}
+                </td>
+                <td>
+                  <button onClick={() => deleteNote(index)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
         {Array.from({ length: Math.ceil(filteredBySearch.length / notesPerPage) }, (_, index) => (
           <button key={index} onClick={() => handlePageChange(index + 1)}>
             {index + 1}
